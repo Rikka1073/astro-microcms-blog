83e3380e1c41de535eff2bf2095fb06cef877c53
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-12-06 21:51:23 +0900

    add:filterする処理の追加

--- src/components/react/ArticlesList.tsx
+++ src/components/react/ArticlesList.tsx
@@ -1,12 +1,37 @@
+import { useState } from "react";
 import Card from "../react/Card";
 import Filter from "./Filter";
 
+type Tags = {
+  name: string;
+  versions: string[];
+};
+
+type Categories = {
+  name: string;
+  tags: Tags;
+};
+
 const ArticlesList = ({ articles, categories }) => {
+  const [filter, setFilter] = useState<any>(articles);
+
+  const handleFilter = (category: string) => {
+    const filteredArticles = articles.filter((article: any) => {
+      return article.tags.some((tag: any) => tag.name === category);
+    });
+    setFilter(filteredArticles);
+  };
+
+  const handleFilterReset = () => {
+    setFilter(articles);
+    console.log("リセット");
+  };
+
   return (
     <>
-      <Filter categories={categories} />
+      <Filter categories={categories} handleFilter={handleFilter} handleFilterReset={handleFilterReset} />
       <div className="sm:grid sm:grid-cols-2 md:grid-cols-3 sm:gap-5 lg:grid-cols-4">
-        {articles.map((article: any) => (
+        {filter.map((article: any) => (
           <Card title={article.title} tags={article.tags} url={article.url} />
         ))}
       </div>
--- src/components/react/Filter.tsx
+++ src/components/react/Filter.tsx
@@ -1,11 +1,10 @@
-const Filter = ({ categories }) => {
-  console.log("reactテスト", categories);
+const Filter = ({ categories, handleFilter, handleFilterReset }) => {
   return (
     <div>
       <form className="filter mb-4 md:mb-6">
-        <input className="btn btn-square" type="reset" value="×" />
-        {categories.map((categorie, index) => (
-          <input key={index} className="btn mb-1" type="radio" name="categories" aria-label={categorie} />
+        <input className="btn filter-reset" type="radio" name="categories" aria-label="All" onChange={handleFilterReset} />
+        {categories.map((category, index) => (
+          <input key={index} className="btn mb-1" type="radio" name="categories" aria-label={category} onChange={() => handleFilter(category)} />
         ))}
       </form>
     </div>
