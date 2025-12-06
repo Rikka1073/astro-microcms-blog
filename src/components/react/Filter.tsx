e6327ed818f6b8434769b38f614e2b35e7d89e30
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-12-06 13:25:31 +0900

    update:Filter,Headerコンポーネントのマージンの追加

--- src/components/react/Filter.tsx
+++ src/components/react/Filter.tsx
@@ -2,7 +2,7 @@ const Filter = ({ categories }) => {
   console.log("reactテスト", categories);
   return (
     <div>
-      <form className="filter mb-4">
+      <form className="filter mb-4 md:mb-6">
         <input className="btn btn-square" type="reset" value="×" />
         {categories.map((categorie, index) => (
           <input key={index} className="btn mb-1" type="radio" name="categories" aria-label={categorie} />
